87b004441c3b31b84e6379b8487aa20b7607b411
eunhye3333 <eunhye_3@naver.com>
2021-08-21 13:19:02 +0900

    for문 완료 - 버퍼 제외

---
+++ 03_stage3/src/com/baekjoon/stage03/run/Run.java
@@ -0,0 +1,20 @@
+package com.baekjoon.stage03.run;
+
+import com.baekjoon.stage03.ForStatement;
+
+public class Run {
+	public static void main(String[] args) {
+		ForStatement fs = new ForStatement();
+//		fs.no2739();
+//		fs.no10950();
+//		fs.no8393();
+		
+//		fs.no2741();
+//		fs.no2742();
+//		fs.no11021();
+		fs.no11022();
+//		fs.no2438();
+//		fs.no2439();
+//		fs.no10871();
+	}
+}
