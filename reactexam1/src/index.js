403e13260e3a2c9d9336211e991a4c2bc3a22462
eunhye <eunhye_3@naver.com>
2022-05-09 17:22:13 +0900

    JSX

--- reactexam1/src/index.js
+++ reactexam1/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
+// import MyFooter from "./MyFooter";
+
 const root = ReactDOM.createRoot(document.getElementById("root")); // App 함수가 리턴하는 값(<App />)을 ID가 root인 요소 아래로 넣겠다
 root.render(
   <React.StrictMode>
