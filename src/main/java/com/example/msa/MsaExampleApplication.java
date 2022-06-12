0a2f541070a7c87ce7c1826dc15a88b0e09c03bb
NaEunHye <eunhye_3@naver.com>
2022-06-12 20:30:27 +0900

    msa 프로젝트 생성

---
+++ src/main/java/com/example/msa/MsaExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.msa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsaExampleApplication.class, args);
+    }
+
+}
